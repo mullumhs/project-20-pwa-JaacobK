081f9a962ec5fc42c2265253fbd6243e5c401475
JaacobK <140124151+JaacobK@users.noreply.github.com>
2024-11-20 10:39:32 +1100

    data model done

--- models.py
+++ models.py
@@ -2,5 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Collum(db.String(100), nullable = False)
+    developer = db.Collum(db.String(100))
+    publisher = db.Collum(db.String(100))
+    realese = db.Collum(db.Int)
+    genre = db.Collum(db.String(100))
+    image_url = db.Collum(db.String(100))
+
 # Define your database model here
 # Example: class Item(db.Model):
