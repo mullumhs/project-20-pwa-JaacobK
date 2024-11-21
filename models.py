52eddf6a9e47300b8ae2661e72781c986ff9baaa
JaacobK <140124151+JaacobK@users.noreply.github.com>
2024-11-22 09:45:04 +1100

    nav bar done

--- models.py
+++ models.py
@@ -4,12 +4,12 @@ db = SQLAlchemy()
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Collum(db.String(100), nullable = False)
-    developer = db.Collum(db.String(100))
-    publisher = db.Collum(db.String(100))
-    realese = db.Collum(db.Int)
-    genre = db.Collum(db.String(100))
-    image_url = db.Collum(db.String(100))
+    title = db.Column(db.String(100), nullable = False)
+    developer = db.Column(db.String(100))
+    publisher = db.Column(db.String(100))
+    realese = db.Column(db.Integer)
+    genre = db.Column(db.String(100))
+    image_url = db.Column(db.String(100))
 
 # Define your database model here
 # Example: class Item(db.Model):
