22d4ba2d58b21047dd424331ca024343cb95e2c6
JaacobK <140124151+JaacobK@users.noreply.github.com>
2024-12-02 09:50:25 +1100

    add modal layout done

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, flash
-from models import db # Also import your database model here
+from models import db, Game # Also import your database model here
 
 # Define your routes inside the 'init_routes' function
 # Feel free to rename the routes and functions as you see fit
@@ -13,15 +13,14 @@ def init_routes(app):
     def get_items():
         # This route should retrieve all items from the database and display them on the page.
         return render_template('index.html', message='Displaying all items')
-
-
-
+    
     @app.route('/add', methods=['POST'])
-    def create_item():
-        # This route should handle adding a new item to the database.
-        return render_template('index.html', message='Item added successfully')
-
-
+    def add_item():
+        new_game = Game(
+            
+        )
+        # This route should handle updating an existing item identified by the given ID.
+        return render_template('index.html', message=f'Item added successfully')
 
     @app.route('/update', methods=['POST'])
     def update_item():
