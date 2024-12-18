2fdaa1ce0df8be0e5ff8c8aa9bcc4770d4aba328
JaacobK <140124151+JaacobK@users.noreply.github.com>
2024-12-18 22:26:15 +1100

    Revert "revert????"
    This reverts commit 1fd540bfd9f86d52454b700177cbe9a9307466f0.

--- views.py
+++ views.py
@@ -19,7 +19,7 @@ def init_routes(app):
 
         if search_query:
             games = Game.query.filter(Game.title.ilike(f'%{search_query}%')).all()
-            return render_template('index.html', games= filter_games)
+            return render_template('index.html', games= games)
         if genre:
             filter_games = Game.query.filter(Game.genre.ilike(f'%{genre}%')).all()
             return render_template('index.html', games= filter_games)
@@ -27,7 +27,6 @@ def init_routes(app):
             games=Game.query.all()
          # This route should retrieve all items from the database and display them on the page.
             return render_template('index.html', games= games)
-<<<<<<< HEAD
     """
     @app.route('/view', methods=['GET'])
     def view():
@@ -36,9 +35,6 @@ def init_routes(app):
         return render_template('index.html', games= game)
         """
 
-=======
-    
->>>>>>> parent of 86af4c0 (try to add view function fixed little error in searching, search and ilter now work)
     @app.route('/add', methods=['POST'])
     def add_item():
         new_game = Game(
